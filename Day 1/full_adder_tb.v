e4b8138bc4c6df9283859a3dc82540658db191fa
Hitesh Paliwal <108817818+Hitesh2598@users.noreply.github.com>
2023-03-09 13:50:10 +0530

    Create full_adder_tb.v

---
+++ Day 1/full_adder_tb.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:28:15 02/22/2023 
+// Design Name: 
+// Module Name:    halfadderB 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
