ebe70ae4e542f1f7fed2ff35848401c80fe8b125
Hitesh Paliwal <108817818+Hitesh2598@users.noreply.github.com>
2024-07-12 22:11:34 +0530

    Update freq_div_4.v

--- Day 2/freq_div_4.v
+++ Day 2/freq_div_4.v
@@ -1,1 +1,37 @@
 
+module clk_div(clk_in,enable,reset,clk_out);
+  input clk_in;
+  input enable;
+  input reset;
+  output reg clk_out;
+  
+  reg clk_div2;
+
+  always @(posedge clk_in or posedge reset)
+
+    if(reset)
+      begin
+        clk_div2 <= 0;
+       end
+  
+   else if (enable)
+     begin     
+        clk_div2 <= ~clk_div2 ;
+      end
+  
+  always @(posedge clk_div2 or posedge reset)
+    
+	begin
+      
+      if(reset)
+        begin
+	     	clk_out <= 0;
+        end
+      
+      else if(enable)
+        	begin
+              clk_out <= ~clk_out ;
+            end
+    end
+      
+endmodule
