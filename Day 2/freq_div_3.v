92ba0ec3f4b283b6630687490790581c4e95c91f
Hitesh Paliwal <108817818+Hitesh2598@users.noreply.github.com>
2024-07-12 22:49:50 +0530

    Update freq_div_3.v

--- Day 2/freq_div_3.v
+++ Day 2/freq_div_3.v
@@ -1,1 +1,36 @@
+module clk_div (
+    input clk_in,     // input clock
+    input reset,   // reset signal
+    output reg clk_out  // divided clock output
+);
 
+  reg [1:0] pos_cnt;
+  reg [1:0] neg_cnt;
+
+  always @(posedge clk_in or posedge reset)
+    begin
+    if (reset) 
+        pos_cnt <= 0;
+    
+      else if (pos_cnt==2) 
+          pos_cnt <= 0;
+      else 
+        pos_cnt <= pos_cnt + 1;
+    end
+        
+  
+  always @(negedge clk_in or posedge reset)
+    begin
+    if (reset) 
+        neg_cnt <= 0;
+    
+      else if (neg_cnt==2) 
+          neg_cnt <= 0;
+      else 
+        neg_cnt <= neg_cnt + 1;
+    end
+
+  
+  assign clk_out = ((pos_cnt ==2) | (neg_cnt ==2));
+  
+endmodule
