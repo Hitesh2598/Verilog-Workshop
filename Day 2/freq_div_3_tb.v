63773ba3034a3a88f1467d38aec9294ed4f66b3a
Hitesh Paliwal <108817818+Hitesh2598@users.noreply.github.com>
2024-07-12 22:50:41 +0530

    Update freq_div_3_tb.v

--- Day 2/freq_div_3_tb.v
+++ Day 2/freq_div_3_tb.v
@@ -1,1 +1,38 @@
-
+module tb;
+  
+  reg clk_in , enable, reset;
+  wire clk_out;
+  
+  clk_div dut(clk_in,reset,clk_out);
+  
+  always #5 clk_in=~clk_in;
+    
+  initial
+    begin
+      clk_in <= 0;
+      reset <= 0 ;
+      enable <= 0;
+      
+  
+  #10 reset <= 1;
+  #20 reset <= 0;
+      enable <= 1;
+  #200 $finish;
+  
+    end
+  
+ initial 
+   
+   begin
+     $monitor("clk_in = %b , reset = %b , clk_out = %b ,enable = %b ",clk_in,reset,clk_out,enable);
+   
+   end
+  
+    initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars;
+    end
+  
+  
+endmodule
